0ad8aa0897cda7649fa196cc971d6afbbd285a49
Viet Anh <vietanhtong1508@gmail.com>
2017-11-19 16:08:00 +0700

    Initial commit

---
+++ src/main/java/com/example/mycontact/MyContactApplication.java
@@ -0,0 +1,12 @@
+package com.example.mycontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyContactApplication.class, args);
+	}
+}
